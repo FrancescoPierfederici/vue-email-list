942379d85b9f89fe4c76481b4a047b628d8266ca
FrancescoPierfederici <francescopierf@gmail.com>
2021-05-11 16:54:07 +0200

    commit

---
+++ myscript.js
@@ -0,0 +1,46 @@
+// Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
+
+
+
+
+new Vue({
+    el: '#app',
+    data: {
+        emails: [],
+       newEmail:[],
+
+    },
+    mounted() {
+       const loopTimes = 10
+       
+        for (let i = 0; i < loopTimes; i++) {
+            axios.get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then((resp) => {
+                    setTimeout(() => {
+                        this.emails.push(resp.data.response);
+                        // console.log(resp);
+                        // let casual = Math.floor(Math.random() * this.email.length);
+                        // this.randomEmail.push(casual);
+
+                        //QUESTA PARTE DA RIVEDERE 
+
+                        if (this.emails.length === loopTimes) {
+                            this.newEmail.push(...this.emails);
+                            
+                        }
+                    },Math.ceil(Math.random() * 1000));
+                  
+
+
+                })
+
+        }
+        // this.newEmail = this.emails
+
+
+    },
+})
+
